f57905d657b97e2e1227bf3ef9e79c0c2ece4796
Charles <setpixelphone@gmail.com>
2015-08-21 02:26:59 -0400

    deploying and gulp working

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,6 +4,9 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var browserSync = require('browser-sync').create();
 var parameters = require('../config/parameters.js');
+var awsconfig = require('../config/awsconfig.json');
+var awspublish = require('gulp-awspublish');
+
 var gulpLoadPlugins = require('gulp-load-plugins');
 var $ = gulpLoadPlugins();
 
@@ -15,13 +18,13 @@ gulp.task('browser-sync', ['styles'], function() {
 	browserSync.init({
 		port: 8000,
 		server: {
-			basedir: parameters.app_url
+			baseDir: parameters.web_path
 		}
 	});
 });
 
 gulp.task('move-assets', function() {
-	return gulp.src([parameters.app_path + '/assets/img/*', parameters.app_path + '/assets/fonts/*/**'], {
+	return gulp.src([parameters.app_path + '/index.html', parameters.app_path + '/assets/asyncjs/*', parameters.app_path + '/assets/img/*', parameters.app_path + '/assets/fonts/*/**'], {
 		base: parameters.app_path
 	})
 	.pipe(gulp.dest(parameters.web_path));
@@ -83,5 +86,35 @@ gulp.task('watch', function() {
 	gulp.watch(parameters.assets_path + '/**/*.scss', ['styles']);
 	gulp.watch(parameters.app_path + '/**/*.html', ['bs-reload']);
 });
+
+gulp.task('publish', function() {
+ 
+  // create a new publisher using S3 options 
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property 
+  var publisher = awspublish.create(awsconfig);
+ 
+  // define custom headers 
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+    // ... 
+  };
+ 
+  return gulp.src('public/**')
+     // gzip, Set Content-Encoding headers and add .gz extension 
+    .pipe(awspublish.gzip())
+    //.pipe(awspublish.gzip({ ext: '.gz' }))
+ 
+    // publisher will add Content-Length, Content-Type and headers specified above 
+    // If not specified it will set x-amz-acl to public-read by default 
+    .pipe(publisher.publish(headers))
+ 
+    // create a cache file to speed up consecutive uploads 
+    .pipe(publisher.cache())
+ 
+     // print upload updates to console 
+    .pipe(awspublish.reporter());
+});
+
+
 
 gulp.task('default', ['move-assets', 'styles', 'minify', 'watch', 'browser-sync']);
